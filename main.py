f77121f152db85f33d34477a814fdafdca55ad42
afreelancer1 <56665706+afreelancer1@users.noreply.github.com>
2020-10-02 12:06:23 -0700

    Added simple machine learning to the game

--- main.py
+++ main.py
@@ -2,37 +2,48 @@
 
 import random
 
-board = []
 
-for i in range(3):
-	board.append([1,1,1])
+ai = {}
+steps = ""
+board = [[1, 1, 1], [1, 1, 1], [1, 1, 1]]
 
 def enter():
 	'''Getting user's move'''
+	global steps
 	try:
 		inp = input("Enter Row and Column: ")
 		r, c = int(inp[0])-1, int(inp[1])-1
 		if board[r][c] == 1:
 			board[r][c] = 0
+			steps = steps + str(r) + str(c)
+			move()
 		else:
 			print("Move not allowed")
 			enter()
 	except:
 		print("Invalid Input")
 		enter()
-	move()
 	draw()
 
 def move():
 	'''Getting AI move'''
-	try:
-		r, c = random.randint(0,2), random.randint(0,2)
-		if board[r][c] == 1:
-			board[r][c] = 2
+	global steps
+	r, c = random.randint(0,2), random.randint(0,2)
+	if board[r][c] == 1:
+		step = steps + str(r) + str(c)
+		if step in ai and not (ai[step] is None):
+			if ai[step] >= 0:
+				board[r][c] = 2
+			else:
+				move()
 		else:
-			move()
-	except:
-		print("Game Over!")
+			ai[step] = 0
+			board[r][c] = 2
+		steps = step
+	elif len(steps)==18:
+		check()
+	else:
+		move()
 
 def draw():
 	'''Drawing the board'''
@@ -44,16 +55,46 @@ def draw():
 	if not result == 1:
 		enter()
 
+
 def check():
 	'''Checking for Victory'''
+	global steps, board
 	for i in range(3):
 		if (sum(board[i]) == 6) or (board[0][i]+board[1][i]+board[2][i] == 6) or (board[0][0]+board[1][1]+board[2][2] == 6) or (board[0][2]+board[1][1]+board[2][0] == 6):
 			print("You lose!")
+			while len(steps)>0:
+				ai[steps] += 0.5
+				steps = steps[:-4]
+			next_game()
 			return 1
 			break
 		if (sum(board[i]) == 0) or (board[0][i]+board[1][i]+board[2][i] == 0) or (board[0][0]+board[1][1]+board[2][2] == 0) or (board[0][2]+board[1][1]+board[2][0] == 0):
 			print("You win!")
+			while len(steps)>0:
+				ai[steps] += -0.3
+				steps = steps[:-4]
+			next_game()
 			return 1
 			break
+	if len(steps)==18:
+		print("Draw!")
+		while len(steps)>0:
+			ai[steps] += 0.1
+			steps = steps[:-4]
+		next_game()
+		return 1
+
+def next_game():
+	global board
+	board = [[1, 1, 1], [1, 1, 1], [1, 1, 1]]
+	again = input("Play again?")
+	yes = ["yes","Yes","Y","y"]
+	for i in yes:
+		if again == i:
+			draw()
+			break
+	else:
+		print("Thanks for playing!")
+		exit()
 
 draw()
